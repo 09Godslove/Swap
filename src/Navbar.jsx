1e8827f6ede82e007e92e3c8c96e2c885901d653
Godslove Uwumwonse <godsloveaiyowie@gmail.com>
2023-09-11 13:50:32 +0100

    navbar completed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
 import './styles/navbar.css'
 
 export default function Navbar(){
     let navigate = useNavigate()
+    const [scrolled,setScrolled]=React.useState(false);
+    const handleScroll=() => {
+        const offset=window.scrollY;
+        if(offset > 0.5 ){
+            setScrolled(true);
+        }
+        else{
+            setScrolled(false);
+        }
+    }
+
+  useEffect(() => {
+    window.addEventListener('scroll',handleScroll)
+  })
+    let navbarClasses=['Navbar'];
+        if(scrolled){
+            navbarClasses.push('scrolled');
+    }
     return(
-        <div className='Navbar'>
+        <div className={navbarClasses.join(" ")}>
             <div className='logo'>
                 <img src='./src/assets/Swap-Logo.svg' alt='logo' />
                 <p>SWAP</p>
