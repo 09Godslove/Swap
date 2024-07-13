770b9cea725feb1f2ea083e552ad0daec3c7fc6c
Godslove Uwumwonse <godsloveaiyowie@gmail.com>
2024-07-13 21:41:13 +0100

    again with one more

--- src/View.jsx
+++ src/View.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from "./NavbarOne";
-import "./styles/View.css";
+import "./styles/view.css";
 
 function View() {
   const { id } = useParams();
