3bc8c20765657493fe5df4db8e7b93607a596ee0
Godslove Uwumwonse <godsloveaiyowie@gmail.com>
2024-07-13 21:35:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import ListItem from './pages/ListItem'
-import View from './pages/View' 
+import View from './View' 
 import ErrorPage from './pages/ErrorPage'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
